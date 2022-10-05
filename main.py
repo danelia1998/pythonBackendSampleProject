19877a066997abe87d35d35f8a565e537ae6a68f
David Danelia <davit.danelia.1@btu.edu.ge>
2022-10-05 11:34:00 +0400

    Adding sample backend job for generating json cv from endpoints payload

--- main.py
+++ main.py
@@ -1,39 +1,63 @@
 from fastapi import FastAPI, File, Form, UploadFile
 from pydantic import BaseModel
-import pickle
-import numpy as np
-import pandas as pd
 from io import StringIO
+import json
 
 app = FastAPI()
 
-class IrisSpecies(BaseModel):
-    sepal_length: float 
-    sepal_width: float 
-    petal_length: float 
-    petal_width: float
+first_part = []
+second_part = []
+third_part = []
+final_array = []
 
-@app.post('/predict')
-async def predict_species(iris: IrisSpecies):
-    data = iris.dict()
-    loaded_model = pickle.load(open('LRClassifier.pkl', 'rb'))
-    data_in = [[data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']]]
-    prediction = loaded_model.predict(data_in)
-    probability = loaded_model.predict_proba(data_in).max()
-            
-    return {
-        'prediction': prediction[0],
-        'probability': probability
+@app.post('/{category}/')
+async def main_structure(category: str, name: str, label: str, email: str, phone: str, website: str, summary: str):
+    testvar1 = {
+        category: {
+            'name': name,
+            'label': label,
+            'email': email,
+            'phone': phone,
+            'website': website,
+            'summary': summary,
+        }
     }
+    first_part.append(testvar1)
+    return testvar1
 
-@app.post("/files/")
-async def create_file(file: bytes = File(...), token: str = Form(...)):
-    s=str(file,'utf-8')
-    data = StringIO(s)
-    df=pd.read_csv(data)
-    print(df)
-    #return df
-    return {
-        "file": df,
-        "token": token,
+
+@app.post('/work')
+async def works_structure(company: str, position: str, website: str, start_date: str, end_date: str, summary: str):
+    testvar2 = {
+            'company': company,
+            'position': position,
+            'website': website,
+            'start_date': start_date,
+            'end_date': end_date,
+            'summary': summary,
+        }
+    second_part.append(testvar2)
+    return testvar2
+
+
+@app.post('/education')
+async def education_structure(university: str, area: str, study_type: str, start_date: str, end_date: str):
+    testvar3 = {
+        'education': [
+            {
+                'institution': university,
+                'area': area,
+                'study_type': study_type,
+                'start_date': start_date,
+                'end_date': end_date,
+            }
+        ]
     }
+    third_part.append(testvar3)
+    return testvar3
+
+
+@app.get('/cv')
+async def education_structure():
+    final_array = [first_part[0], {'working experience': second_part}, third_part[0]]
+    return final_array
